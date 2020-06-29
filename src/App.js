f70ef04a6a57be35189511deec539ebcdad04a56
secondaryfun <c.birkenhagen@gmail.com>
2020-06-28 22:15:45 -0400

    loading states.  prepping for api pull

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './css/App.css';
+import Map from './components/USAMap'
+import ReactBootstrapSlider from 'react-bootstrap-slider';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-slider/dist/css/bootstrap-slider.css"
+import States from './data/states.json'
+
+
+export default class App extends Component {
+	constructor(props) {
+		super(props)
+		let startDay = 20200401
+
+		this.state = {
+			dailyCovidStats: [],
+			currentDay: startDay,
+			linkStyle: { "textDecoration": "none", "color": "white" },
+			step: 1,
+			max: 20200481,
+			min: startDay,
+		}
+	}
+	convertDay = (minPlusDays) => {
+		let date
+		if (minPlusDays - this.state.min > 61) date = 20200600 + minPlusDays - this.state.min
+		else if (minPlusDays - this.state.min > 30) date = 20200500 + minPlusDays - this.state.min
+		else date = 20200600 + minPlusDays - this.state.min
+
+		return date
+	}
+	componentDidMount() {
+		// this.getData()
+		// this.updateUserCourses()
+	}
+	getData = () => {
+		const url = "https://virus-bloom-api.herokuapp.com/states/" + this.state.currentDay
+
+		fetch(url)
+			.then(res => res.json())
+			.then(result => {
+				this.setState({ dailyCovidStats: result });
+			}).catch(err => console.log(err))
+	}
+	// updateUserCourses = () => {
+	// 	const url = "https://udemy-courses-api.herokuapp.com/courses/category/user"
+
+	// 	fetch(url)
+	// 		.then(res => res.json())
+	// 		.then(result => {
+	// 			console.log('Updated user courses.')
+	// 			this.setState({ userCourseList: result });
+	// 		}).catch(err => console.log(err))
+	// }
+	changeValue = (e) => {
+		this.setState({ currentDay: e.target.value })
+	}
+	render() {
+		return (
+			<div>
+				<div className="body-wrapper">
+					<div className="body-overlay">
+						<header className="logo-header header">
+							<div className="logo-link">
+								<Link to={"/map/"} style={this.state.linkStyle} >
+									<img src="/images/covid-bloom-white.png" alt="covid-bloom-logo" className="header__logo" />
+								</Link>
+							</div>
+							<Link to={"/map/"} style={this.state.linkStyle} >
+								<h3 className="nav-link" >Covid Cases in the US</h3>
+							</Link>
+						</header>
+						<main>
+							<ReactBootstrapSlider
+								value={this.state.currentDay}
+								slideStop={this.changeValue}
+								step={this.state.step}
+								max={this.state.max}
+								min={this.state.min}
+								orientation="horizontal"
+								// reversed={true}
+								disabled="enabled" />
+							<Map />
+						</main>
+					</div>
+				</div>
+				<footer className="footer">
+					<p>2020 RedCap Concepts.</p>
+				</footer>
+			</div>
+		)
+	}
 }
-
-export default App;
